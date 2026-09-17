4b91bd166310bbd27afc690b7e003668f1d92611
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-09-16 21:12:37 -0300

    feat:admin

---
+++ biblioteca/acervo/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Livro
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = (
+        'titulo',
+        'autor',
+        'ano',
+        'disponivel',
+    )
+
+    search_fields = (
+        'titulo',
+        'autor',
+    )
+
+    list_filter = (
+        'disponivel',
+        'ano',
+    )
